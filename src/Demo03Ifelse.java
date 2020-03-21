2badff0d823da415150ee54fea586cffb3df0a63
liuyuanyuan-1993 <55902215+liuyuanyuan-1993@users.noreply.github.com>
2020-03-21 15:22:47 +0800

    if else 判断奇偶数

---
+++ src/Demo03Ifelse.java
@@ -0,0 +1,16 @@
+
+public class Demo03Ifelse {
+
+	public static void main(String[] args) {
+	int num =5;
+	num=10;
+	if(num%2==0) {
+		System.out.println("Å¼Êý");
+	
+	}
+	else {
+		System.out.println("ÆæÊý");
+	}
+	}
+
+}
