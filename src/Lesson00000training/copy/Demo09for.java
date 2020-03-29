369b6969d2a802fa95e5ceee5ee4dea4e39863e7
liuyuanyuan-1993 <55902215+liuyuanyuan-1993@users.noreply.github.com>
2020-03-29 15:44:06 +0800

    for循环格式
    for（初始化语句;条件判断;步进语句）{
    循环体}
    最后的输出语句

---
+++ src/Lesson00000training/copy/Demo09for.java
@@ -0,0 +1,11 @@
+package Lesson00000training.copy;
+
+public class Demo09for {
+	public  static void main (String[]args) {
+	for(int i=1;i<=100;i++) {
+		System.out.println("我错啦"+i);
+	}
+	System.out.println("程序停止");
+}
+
+}
