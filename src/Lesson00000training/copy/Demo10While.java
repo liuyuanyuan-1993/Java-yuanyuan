65a262f4b5df64bfd47ab542bd2b9410fa45d7bb
liuyuanyuan-1993 <55902215+liuyuanyuan-1993@users.noreply.github.com>
2020-04-01 22:09:41 +0800

    for循环 和 while循环
    while标准格式：
    while（条件判断）{
    循环体
    }
    
    while循环的扩展格式：
    初始化语句
    while（条件判断）{
    循环体
    步进语句
    }

---
+++ src/Lesson00000training/copy/Demo10While.java
@@ -0,0 +1,20 @@
+package Lesson00000training.copy;
+
+public class Demo10While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=1;i<=10;i++) {
+	System.out.println("我错啦"+i);
+	
+}
+System.out.println("========================");
+
+int i=1; //初始化语句
+while(i<=10) { //条件判断
+	System.out.println("我错啦"+i); //循环体
+	i++; //4.步进语句
+}
+	}
+	
+}
