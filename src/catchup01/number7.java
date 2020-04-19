c1f22e3aca6a42aa840db9eb37016cc4e5e9c70c
liuyuanyuan-1993 <55902215+liuyuanyuan-1993@users.noreply.github.com>
2020-04-19 10:12:06 +0800

    if 打印100以内7的倍数

---
+++ src/catchup01/number7.java
@@ -0,0 +1,21 @@
+package catchup01;
+
+public class number7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+for(int i=1;i<=100;i++) {
+	
+	//正常写if函数时不加‘；’的 
+	//if(i%7==0)；把这句话给结束了。加了分号上下变得没有关系了。
+	
+	if(i%7==0){
+	System.out.println(i);
+	
+	
+	}
+}
+	}
+
+}
