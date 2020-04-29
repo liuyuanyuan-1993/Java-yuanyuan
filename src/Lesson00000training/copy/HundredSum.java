1b95a5a3f4d1630ae2af41b11577e7db515dbde4
liuyuanyuan-1993 <55902215+liuyuanyuan-1993@users.noreply.github.com>
2020-04-29 16:43:06 +0800

    for循环求和

---
+++ src/Lesson00000training/copy/HundredSum.java
@@ -0,0 +1,16 @@
+package Lesson00000training.copy;
+
+public class HundredSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int sum =0;
+
+for(int i=1;0<i&&i<=100;i++) {
+	if(i % 2==0);
+	sum+=i;
+}
+	System.out.println("和为："+ sum);
+	}
+
+}
