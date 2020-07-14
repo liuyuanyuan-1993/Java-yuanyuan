5502fbd0a98881cf8e8c7ebad5b220946d07356e
liuyuanyuan-1993 <55902215+liuyuanyuan-1993@users.noreply.github.com>
2020-07-14 18:39:23 +0800

    打印出数组的值
    1.定义一个10位数组，使其存储1,2,3,4,5…并且打印出来

---
+++ src/lessonnew1/practice1.java
@@ -0,0 +1,15 @@
+package lessonnew1;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] arr=new int[10];
+ arr[0]=1;
+for(int i=1;i<=arr.length;i++) {
+	System.out.println(i);
+}
+	
+	}
+
+}
