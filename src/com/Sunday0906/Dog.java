b244079b6d80056148ffcd1f98e7f36134c30abd
liuyuanyuan-1993 <55902215+liuyuanyuan-1993@users.noreply.github.com>
2020-09-06 13:19:04 +0800

    动物类2

---
+++ src/com/Sunday0906/Dog.java
@@ -0,0 +1,11 @@
+package com.Sunday0906;
+
+public class Dog extends Animal{
+  public void call() {
+	  System.out.println("狗叫");
+  }
+  
+  public void eatbone() {
+	  System.out.println("狗吃骨头");
+  }
+}
