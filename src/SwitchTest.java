26652edefd709410c28a6e5c89b80a0c6e72433b
liuyuanyuan-1993 <55902215+liuyuanyuan-1993@users.noreply.github.com>
2020-09-20 17:38:47 +0800

    switch语句 选择结构
    case表达式只决定程序入口 不能决定程序出口 除非加break跳出

---
+++ src/SwitchTest.java
@@ -0,0 +1,13 @@
+
+public class SwitchTest {
+  public static void main(String[] args) {
+	int a = 2;
+	switch(a) { 
+	case 1 :System.out.println("进行加法运算");
+	case 2 :System.out.println("进行减法运算");
+	case 3 :System.out.println("进行乘法运算");
+	case 4 :System.out.println("进行除法运算");break;
+	default :System.out.println("a的值不合法");
+	}
+}
+}
