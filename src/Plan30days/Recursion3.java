b6b92f2141666109bf0cfb014db6bbe1efff9ce0
liuyuanyuan-1993 <55902215+liuyuanyuan-1993@users.noreply.github.com>
2020-09-24 23:00:06 +0800

    Recursion 迭代
    https://www.hackerrank.com/challenges/30-recursion/problem

---
+++ src/Plan30days/Recursion3.java
@@ -0,0 +1,39 @@
+package Plan30days;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Recursion3 {
+
+    // Complete the factorial function below.
+    static int factorial(int n) {
+    	int result =1;
+    	for(int i=1;i<=n;i++) {
+    		result = result*i;
+    	}
+return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = factorial(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
