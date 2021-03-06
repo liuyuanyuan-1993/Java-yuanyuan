a6228a97c5239041057f9c0ad041018ebc57045c
liuyuanyuan-1993 <55902215+liuyuanyuan-1993@users.noreply.github.com>
2021-03-06 15:16:55 +0800

    连接我自己的数据库，插入honor手机

--- src/catchup20/MysqlTest.java
+++ src/catchup20/MysqlTest.java
@@ -9,14 +9,14 @@ public class MysqlTest {
 	public static void main(String[] args) throws ClassNotFoundException {
 		// load mysql driver
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		try (
+		try {
 				//setup database connection
-				Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.1.109:3306/sales", "root", "123456");
+				Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sales", "root", "123456");
 				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery("select * from product_tbl");) {
-			while (rs.next()) {
-				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getDouble(3) + " " + rs.getDate(4));
-			}
+ int num = stmt.executeUpdate("insert into product_tbl"+ "(product_name,"+ "price,"
+ + "onboard_date,produce_city)values('Honor','2999.0','2020-10-01','Dalian');");
+ //看是否插入进去了honor
+ //System.out.println("Insert rows: " + num);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}	
