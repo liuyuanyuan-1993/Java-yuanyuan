70f8f13e2e99240fb1f80f9930d79094db481914
liuyuanyuan-1993 <55902215+liuyuanyuan-1993@users.noreply.github.com>
2021-03-07 14:27:38 +0800

    插入一行新的 连接数据库

--- src/jdbc20210307/EmpDao.java
+++ src/jdbc20210307/EmpDao.java
@@ -1,29 +1,42 @@
 package jdbc20210307;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class EmpDao {
-  //删除
-  public int delete(int empno) {
+  //插入
+  public int insert(Emp emp) {
 	  Connection conn = ConnDB.getConnection();
 	  //主语sql之间的空格 无效sql语句
-	  String sql="delete from emps where empno="+empno;
+	 // String sql="delete from emps where empno="+empno;
 	  //载体
-	  Statement st=null;
-	  
+	  PreparedStatement pst=null;
+	  String sql ="insert into emps(empno,ename,job,sal,hiredate,deptno)values"+
+	  "(?,?,?,?,to_date(?,'yyyy-MM-dd'),?)";
 	  try {
-		st=conn.createStatement();
-		  return st.executeUpdate(sql);
+		pst=conn.prepareStatement(sql);
+		pst.setInt(1,emp.getEmpno());
+		pst.setString(2,emp.getEname());
+		pst.setString(3,emp.getJob());
+		pst.setDouble(4,emp.getSal());
+		pst.setString(5,emp.getHiredate());
+		pst.setInt(6,emp.getDeptno());
+		
+		  return pst.executeUpdate();
 	} catch (SQLException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}finally {
 		try {
-			if(st!=null) {
-				st.close();
+			if(pst!=null) {
+				pst.close();
 			}
+			if(conn!=null) {
+				conn.close();
+			}
+				
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -36,7 +49,14 @@ public class EmpDao {
   public static void main(String[] args) {
 	  EmpDao empDao =new EmpDao();
 	  int empno=7369;
-	  System.out.println(empDao.delete(empno));
-	  
+	// System.out.println(empDao.insert(empno));
+	  Emp emp =new Emp();
+	  emp.setEmpno(empno);
+	  emp.setDeptno(10);
+	  emp.setEname("TEST");
+	  emp.setJob("SALESMAN");
+	  emp.setSal(888.88);
+	  emp.setHiredate("2021-3-7");
+	  System.out.println(empDao.insert(emp));
   }
 }
