6e4e9647e0e5f6aad8a623dc1df41420396e756b
wanzi <1628062273@qq.com>
2022-01-08 16:16:36 +0800

    Update Scores.java
    数据测试：正确数据，边界数据，错误数据都测到，才算是健壮的程序

--- src/fighting/Scores.java
+++ src/fighting/Scores.java
@@ -8,7 +8,12 @@ public class Scores {
 		// TODO Auto-generated method stub
 			Scanner sc =new Scanner(System.in);
 			int score = sc.nextInt();
-			if(score>=95&&score <=100) {
+			
+	 //数据测试：正确数据，边界数据，错误数据都测到，才算是健壮的程序
+			
+			if(score>100 ||score<0) {
+				System.out.println("你输入的数据有误");
+			}else if(score>=95&&score <=100) {
 				System.out.println("买个自行车");
 			}else if(score>=90&&score <95) {
 				System.out.println("去游乐场玩");
