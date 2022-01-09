6d6caa994ae3f99ba47a6e8abcebef7cbaad8658
wanzi <1628062273@qq.com>
2022-01-09 13:52:07 +0800

    Create While.java
    for 和 while判断

---
+++ src/fighting/While.java
@@ -0,0 +1,26 @@
+package fighting;
+
+public class While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//初始化语句，条件判断语句，条件控制语句
+		for(int i=1;i<=5;i++) {
+			//循环体语句
+			System.out.println("Hello World");
+		}
+		    System.out.println("=================");
+		    
+		//初始化语句
+		int j=1;
+		//条件判断语句
+		while(j<=5) {
+			//循环体语句
+			System.out.println("Hello World");
+			//条件控制语句
+			j++;
+			
+		}
+	}
+
+}
