8899aeb33d42593053cd863452732e82eb148546
wanzi <1628062273@qq.com>
2022-01-09 11:35:29 +0800

    Create OneFive.java
    for循环操作在控制台打印1-5和5-1两种

---
+++ src/fighting/OneFive.java
@@ -0,0 +1,19 @@
+package fighting;
+
+public class OneFive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//输出1-5
+		for(int i =1;i<=5;i++) {
+			System.out.println(i);
+		}
+	//	==================
+		//输出5-1
+		for(int i =5;i>=1;i--) {
+			System.out.println(i);
+		}
+	}
+
+}
