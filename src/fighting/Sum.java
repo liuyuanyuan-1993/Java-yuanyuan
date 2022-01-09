b6067823014ae238d84d330d8b8b43be20839901
wanzi <1628062273@qq.com>
2022-01-09 11:44:09 +0800

    Create Sum.java
    利用for循环求出1-100的和

---
+++ src/fighting/Sum.java
@@ -0,0 +1,14 @@
+package fighting;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for(int i=1;i<=100;i++) {
+			sum+=i;	
+		}
+		System.out.println("1-100的和是"+sum);
+	}
+
+}
