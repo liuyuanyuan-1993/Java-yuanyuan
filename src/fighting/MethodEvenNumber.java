2bdf2b5ce5f0c62c06fa40d36d499113db68214e
wanzi <1628062273@qq.com>
2022-01-09 18:32:40 +0800

    Create MethodEvenNumber.java
    无参数的方法调用

---
+++ src/fighting/MethodEvenNumber.java
@@ -0,0 +1,17 @@
+package fighting;
+
+public class MethodEvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		evenNumber();
+	}
+	public static void evenNumber() {
+		int a=10;
+		if(a%2 ==0) {
+			System.out.println(true);
+		}else {
+			System.out.println(true);
+		}
+	}
+}
