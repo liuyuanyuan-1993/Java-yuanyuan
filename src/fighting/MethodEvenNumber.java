cc399b42d890d439a5097730c576c65b52c85c6d
wanzi <1628062273@qq.com>
2022-01-09 18:43:34 +0800

    Update MethodEvenNumber.java
    无参和有参方法的调用

--- src/fighting/MethodEvenNumber.java
+++ src/fighting/MethodEvenNumber.java
@@ -5,13 +5,27 @@ public class MethodEvenNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		evenNumber();
+		//参数是什么类型，就需要给参数赋对应类型的数值
+		int number =13;
+		evenNumber2(number);
 	}
+	//无参数的方法
 	public static void evenNumber() {
 		int a=10;
 		if(a%2 ==0) {
 			System.out.println(true);
 		}else {
-			System.out.println(true);
+			System.out.println(false);
 		}
 	}
-}
+	
+	//有参数的方法
+	public static void evenNumber2(int number) {	
+		if(number%2 ==0) {
+			System.out.println(true);
+		}else {
+			System.out.println(false);
+		}
+	
+      }
+	}
