744dddf919325dd7df750ad749bf77f8bd63d098
wanzi <1628062273@qq.com>
2022-01-09 12:25:55 +0800

    Create WaterFlowerTree.java
    打印所有的水仙花数

---
+++ src/fighting/WaterFlowerTree.java
@@ -0,0 +1,25 @@
+package fighting;
+
+public class WaterFlowerTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//水仙花数：一个三位数，它的个位十位百位的立方数的和，等于这个数本身
+		//打印所有的水仙花数
+		for(int i =100;i<=999;i++) {
+			//取到个位上的数
+			int ge = i%10;
+			//取到十位上的数
+			int shi = i/10%10;
+			//取到百位上的数
+			int bai = i/10/10%10;
+			//如果个位的立方+十位的立方+百位的立方和等于这个数，它就是水仙花数
+			if(ge*ge*ge+shi*shi*shi+bai*bai*bai==i) {
+				System.out.println(i);
+			}
+			
+			
+		}
+	}
+
+}
