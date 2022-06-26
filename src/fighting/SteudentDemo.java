a12444f7b51ba21497e54f04850b5251a1c5633e
wanzi <1628062273@qq.com>
2022-06-26 13:34:19 +0800

    Student類的測試

---
+++ src/fighting/SteudentDemo.java
@@ -0,0 +1,15 @@
+package fighting;
+
+//学生类的测试类
+public class SteudentDemo {
+
+	public static void main(String[] args) {
+  Student s =new Student();
+  s.age = 27;
+  s.name = "翟子路";
+  
+  s.show();
+  
+	}
+
+}
---
+++ src/fighting/Student.java
@@ -0,0 +1,13 @@
+package fighting;
+
+//学生类
+public class Student {
+
+		String name;
+		int age;
+		public void show() {
+			System.out.println(name+","+age);
+		}
+	}
+
+
